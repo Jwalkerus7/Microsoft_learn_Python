940a7467bc85cfc5bb2d21935af5701dfe47e332
jason walker <jwalkerus3@yahoo.com>
2021-12-28 22:06:43 -0600

    used f to format()+

--- python-format-strings/challenge-f.py
+++ python-format-strings/challenge-f.py
@@ -1,4 +1,4 @@
-first_value = '  FIRST challenge         '
+"""first_value = '  FIRST challenge         '
 second_value = '-  second challenge  -'
 third_value = 'tH IR D-C HALLE NGE'
 
@@ -7,12 +7,21 @@ fifth_value = 'fifth'
 sixth_value = 'sixth'
 
 # First challenge
-
+first_value = first_value.strip()
+first_value = first_value.lower()
+first_value = first_value.title()
+first_value = f"{first_value:^30}"
 
 # Second challenge
-
+second_value = second_value.replace("-", " ")
+second_value = second_value.strip()
+second_value = second_value.capitalize()
 
 # Third challenge
+third_value = third_value.replace(" ", "")
+third_value = third_value.replace("-", " ")
+third_value = third_value.swapcase()
+third_value = f"{third_value:^45}"
 
 
 print(first_value)
@@ -20,19 +29,21 @@ print(second_value)
 print(third_value)
 
 # Fourth challenge - use only the print() function (no f-strings)
-
+print(fourth_value, fifth_value, sixth_value, sep="#", end="!")
 
 # Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
+print(f"\n\t{fourth_value}\n\t{fifth_value}\n\t{sixth_value}")
+
+
+
+fourth_value = 'fourth'
+fifth_value = 'fifth'
+sixth_value = 'sixth'"""
 
 
 
 
-
-
-
-
-
-first_value = '  FIRST challenge         '
+"""first_value = '  FIRST challenge samw as above just in single quotes'
 second_value = '-  second challenge  -'
 third_value = 'tH IR D-C HALLE NGE'
 
@@ -65,4 +76,4 @@ print(third_value)
 print(fourth_value, fifth_value, sixth_value, sep='#', end='!')
 
 # Fifth challenge - use only a single print() function. Create tabs and new lines using f-strings.
-print(f'\n\t{fourth_value}\n\t{fifth_value}\n\t{sixth_value}')
+print(f'\n\t{fourth_value}\n\t{fifth_value}\n\t{sixth_value}')"""
